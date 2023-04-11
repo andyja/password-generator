4a640028ac95c63d4c72804557788eeaa3862f1b
Andy Jackson <andypauljackson@gmail.com>
2023-04-11 17:53:05 +0100

    basic functionality working

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+
+let passwordLength = 10;
+let passwordOneEl = document.getElementById("password-one-el");
+let passwordTwoEl = document.getElementById("password-two-el");
+
+function generatePassword() {
+    for (let i = 0; i < passwordLength; i ++) {
+        let index = Math.floor(Math.random() * characters.length)
+        passwordOneEl.textContent += characters[index]
+    }
+    for (let j = 0; j < passwordLength; j ++) {
+        let index = Math.floor(Math.random() * characters.length)
+        passwordTwoEl.textContent += characters[index]
+    }
+}
