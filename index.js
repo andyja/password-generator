d8ec6a8e8f2762ec4e66bf6220412a0527fb5957
Andy Jackson <andypauljackson@gmail.com>
2023-05-09 21:08:04 +0100

    fix

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const numChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const sybmbolChars = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
     "/"];
 let passwordGenerated = false;
-let passwordLength = 15;
 let passwordOneEl = document.getElementById("password-one-el");
-let passwordTwoEl = document.getElementById("password-two-el");
+// let passwordTwoEl = document.getElementById("password-two-el");
 let resetBtn = document.getElementById("reset-btn");
 let lengthResult = document.querySelector("#length");
 let passwordRangeResult = document.querySelector("#password-range-result");
-let numCharsChecked = document.getElementById("numChars-checked");
-let symbolCharsChecked = document.getElementById("symbolChars-checked")
-
+let numCheckBox = document.getElementById("numChars-checked");
+let symCheckBox = document.getElementById("symbolChars-checked");
 let generatePasswordsBtn = document.getElementById("generate-passwords-btn");
 generatePasswordsBtn.addEventListener('click', generatePasswords);
 
@@ -19,56 +17,132 @@ lengthResult.addEventListener('change', (e) => {
     passwordRangeResult.innerText = e.target.value;
     passwordGenerated = false;
     passwordOneEl.textContent = "";
-    passwordTwoEl.textContent = "";
+    // passwordTwoEl.textContent = "";
     });
 resetBtn.addEventListener('click', resetPasswords);
 
 function renderCopyOneBtn(){
     return ` 
-            <button>
+            <button id="copyOne">
                 <span class="copy-detail">
-                    <i id="copyOne" class="fa-solid fa-copy"></i>
+                    <i  class="fa-solid fa-copy"></i>
                 </span> 
-                Copy Password 1 
+                Click To Copy Password 
             </button>
     `;
     }
-function renderCopyTwoBtn(){
-    return ` 
-            <button>
-                <span class="copy-detail">
-                    <i id="copyTwo" class="fa-solid fa-copy"></i>
-                </span> 
-                Copy Password 2 
-            </button>
-    `;
-    }
-
+// function renderCopyTwoBtn(){
+//     return ` 
+//             <button>
+//                 <span class="copy-detail">
+//                     <i id="copyTwo" class="fa-solid fa-copy"></i>
+//                 </span> 
+//                 Copy Password 2 
+//             </button>
+//     `;
+//     }
 function generatePasswords() {
     passwordLength = lengthResult.value;
     if (passwordGenerated === false) {
-        for (let i = 0; i < passwordLength; i ++) {
-            let index = Math.floor(Math.random() * alphaChars.length)
-            passwordOneEl.textContent += alphaChars[index]
+        if (numCheckBox.checked && symCheckBox.checked){
+            let numElementsFromNumCharsArray = 2;
+            let numElementsFromSybmbolCharsArray = 2;
+            let numElementsFromAlphaCharsArray = passwordLength - numElementsFromNumCharsArray - numElementsFromSybmbolCharsArray;
+            let result = '';    
+            for (let i = 0; i < numElementsFromAlphaCharsArray; i++) {
+                let index = Math.floor(Math.random() * alphaChars.length);
+                if (i < alphaChars.length) {
+                    result += alphaChars[index];
+                }
+            }
+            for (let i = 0; i < numElementsFromNumCharsArray; i++) {
+                let index = Math.floor(Math.random() * numChars.length);
+                if (i < numChars.length) {
+                    result += numChars[index];
+                }
+            }
+            for (let i = 0; i < numElementsFromSybmbolCharsArray; i++) {
+                let index = Math.floor(Math.random() * sybmbolChars.length);
+                if (i < sybmbolChars.length) {
+                    result += sybmbolChars[index];
+                }
+            }
+            passwordOneEl.textContent = result;
         }
-        for (let j = 0; j < passwordLength; j ++) {
-            let index = Math.floor(Math.random() * alphaChars.length)
-            passwordTwoEl.textContent += alphaChars[index]
+        else if(numCheckBox.checked){
+            let numElementsFromNumCharsArray = 2;
+            let numElementsFromSybmbolCharsArray = 0;
+            let numElementsFromAlphaCharsArray = passwordLength - numElementsFromNumCharsArray - numElementsFromSybmbolCharsArray;
+            let result = '';
+            for (let i = 0; i < numElementsFromAlphaCharsArray; i++) {
+                let index = Math.floor(Math.random() * alphaChars.length);
+                if (i < alphaChars.length) {
+                    result += alphaChars[index];
+                }
+            }
+            for (let i = 0; i < numElementsFromNumCharsArray; i++) {
+                let index = Math.floor(Math.random() * numChars.length);
+                if (i < numChars.length) {
+                    result += numChars[index];
+                }
+            }
+            passwordOneEl.textContent = result;
         }
-        passwordGenerated = true;
+        else if (symCheckBox.checked){
+            let numElementsFromNumCharsArray = 0;
+            let numElementsFromSybmbolCharsArray = 2;
+            let numElementsFromAlphaCharsArray = passwordLength - numElementsFromNumCharsArray - numElementsFromSybmbolCharsArray;
+            let result = '';
+            for (let i = 0; i < numElementsFromAlphaCharsArray; i++) {
+                let index = Math.floor(Math.random() * alphaChars.length);
+                if (i < alphaChars.length) {
+                    result += alphaChars[index];
+                }
+            }
+            for (let i = 0; i < numElementsFromSybmbolCharsArray; i++) {
+                let index = Math.floor(Math.random() * sybmbolChars.length);
+                if (i < sybmbolChars.length) {
+                    result += sybmbolChars[index];
+                }
+            }
+            passwordOneEl.textContent = result;
+        }
+        else {
+            let numElementsFromNumCharsArray = 0;
+            let numElementsFromSybmbolCharsArray = 0;
+            let numElementsFromAlphaCharsArray = passwordLength - numElementsFromNumCharsArray - numElementsFromSybmbolCharsArray;
+            let result = '';
+            for (let i = 0; i < numElementsFromAlphaCharsArray; i++) {
+                let index = Math.floor(Math.random() * alphaChars.length);
+                if (i < alphaChars.length) {
+                    result += alphaChars[index];
+                }
+            }
+            passwordOneEl.textContent = result;
+        };
+
+        // for (let j = 0; j < passwordLength; j ++) {
+        //     let index = Math.floor(Math.random() * alphaChars.length)
+        //     passwordTwoEl.textContent += alphaChars[index]
+        // }
+        passwordGenerated = !true;
         document.getElementById("copy-password-one-btn").innerHTML = renderCopyOneBtn();
-        document.getElementById("copy-password-two-btn").innerHTML = renderCopyTwoBtn();
-    }   
-}
+        // document.getElementById("copy-password-two-btn").innerHTML = renderCopyTwoBtn();
+    };
+};     
 
 let copyPasswordOneBtn = document.getElementById("copy-password-one-btn");
+// copyPasswordOneBtn.addEventListener('click', function(){
+//     console.log("clicked")
+// });
+
 copyPasswordOneBtn.addEventListener('click', copyTextOne);
 
 async function copyTextOne() {
-    if (passwordGenerated) {
+    if (!passwordGenerated) {
         let copyPasswordOne = document.getElementById("password-one-el").innerHTML;
         document.getElementById("copyOne").classList.add('copied');
-        document.getElementById("copyTwo").classList.remove('copied');
+        // document.getElementById("copyTwo").classList.remove('copied');
         try {
             await navigator.clipboard.writeText(copyPasswordOne);
             alert('Password1 copied to clipboard');
@@ -77,28 +151,29 @@ async function copyTextOne() {
         }
     }
 }
-let copyPasswordTwoBtn = document.getElementById("copy-password-two-btn");
-copyPasswordTwoBtn.addEventListener('click', copyTextTwo);
 
-async function copyTextTwo() {
-    if (passwordGenerated) {
-        let copyPasswordTwo = document.getElementById("password-two-el").innerHTML;
-        document.getElementById("copyTwo").classList.add('copied');
-        document.getElementById("copyOne").classList.remove('copied');
-        try {
-            await navigator.clipboard.writeText(copyPasswordTwo);
-            alert('Password2 copied to clipboard');
-        } catch (err) {
-            alert('Failed to copy: ', err);
-        }
-    }
-}
+// let copyPasswordTwoBtn = document.getElementById("copy-password-two-btn");
+// copyPasswordTwoBtn.addEventListener('click', copyTextTwo);
+
+// async function copyTextTwo() {
+//     if (passwordGenerated) {
+//         let copyPasswordTwo = document.getElementById("password-two-el").innerHTML;
+//         document.getElementById("copyTwo").classList.add('copied');
+//         document.getElementById("copyOne").classList.remove('copied');
+//         try {
+//             await navigator.clipboard.writeText(copyPasswordTwo);
+//             alert('Password2 copied to clipboard');
+//         } catch (err) {
+//             alert('Failed to copy: ', err);
+//         }
+//     }
+// }
 function resetPasswords() {
     passwordGenerated = false;
     passwordOneEl.textContent = "";
-    passwordTwoEl.textContent = "";
-    numCharsChecked.checked = false;
-    symbolCharsChecked.checked = false;
+    // passwordTwoEl.textContent = "";
+    numCheckBox.checked = false;
+    symCheckBox.checked = false;
     document.getElementById("copy-password-one-btn").innerHTML = "";
-    document.getElementById("copy-password-two-btn").innerHTML = "";
-};  
+    // document.getElementById("copy-password-two-btn").innerHTML = "";
+};
